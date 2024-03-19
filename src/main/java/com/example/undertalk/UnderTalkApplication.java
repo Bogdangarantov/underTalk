5b00e0d67d86ee17f111fc03a5ca07b8769dcf22
BogDan <harysh.bogdan@gmail.com>
2024-03-19 19:28:12 +0200

    initial Commit

---
+++ src/main/java/com/example/undertalk/UnderTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.undertalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnderTalkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/undertalk/UnderTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.undertalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnderTalkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
